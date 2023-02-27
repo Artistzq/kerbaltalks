1039633bd5cf0c3afbd9ccda76f9ba3e184edb96
Yaozq <yaozongqing@outlook.com>
2023-02-27 15:19:34 +0800

    Delete demo files

--- src/main/java/com/chinaero/kerbaltalks/dao/AlphaDao.java
+++
@@ -1,7 +0,0 @@
-package com.chinaero.kerbaltalks.dao;
-
-public interface AlphaDao {
-    String select();
-
-
-}
--- src/test/java/com/chinaero/kerbaltalks/KerbaltalksApplicationTests.java
+++ src/test/java/com/chinaero/kerbaltalks/KerbaltalksApplicationTests.java
@@ -1,18 +1,10 @@
 package com.chinaero.kerbaltalks;
 
-import com.chinaero.kerbaltalks.dao.AlphaDao;
-import com.chinaero.kerbaltalks.service.AlphaService;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.test.context.ContextConfiguration;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 @SpringBootTest
 @ContextConfiguration(classes = KerbaltalksApplication.class)
@@ -25,37 +17,4 @@ class KerbaltalksApplicationTests implements ApplicationContextAware {
         this.applicationContext = applicationContext;
     }
 
-    @Test
-    public void testApplicationContext() {
-        System.out.println(applicationContext);
-
-        AlphaDao alphaDao = applicationContext.getBean(AlphaDao.class);
-        System.out.printf(alphaDao.select());
-
-        alphaDao = applicationContext.getBean("alphaDaoHibernateImpl", AlphaDao.class);
-        System.out.printf(alphaDao.select());
-    }
-
-    @Test
-    public void testBeanManagement() {
-        AlphaService alphaService = applicationContext.getBean(AlphaService.class);
-
-        System.out.println(alphaService);
-    }
-
-    @Test
-    public void testBeanConfig() {
-        SimpleDateFormat simpleDateFormat = applicationContext.getBean(SimpleDateFormat.class);
-        System.out.println(simpleDateFormat.format(new Date()));
-    }
-
-    @Autowired
-    @Qualifier("alphaDaoHibernateImpl")
-    private AlphaDao alphaDao;
-
-    @Test
-    public void testDI() {
-        System.out.println(alphaDao.select());
-        System.out.println(applicationContext.getBean(AlphaDao.class).select());
-    }
 }
