bec7f4f7df9b08794c70d6b4c644ca4d54fccc26
Yaozq <yaozongqing@outlook.com>
2023-03-16 16:42:49 +0800

    feat: add 'browser followers' function

--- src/main/java/com/chinaero/kerbaltalks/contorller/FollowController.java
+++ src/main/java/com/chinaero/kerbaltalks/contorller/FollowController.java
@@ -1,14 +1,22 @@
 package com.chinaero.kerbaltalks.contorller;
 
+import com.chinaero.kerbaltalks.entity.Page;
 import com.chinaero.kerbaltalks.entity.User;
 import com.chinaero.kerbaltalks.service.FollowService;
+import com.chinaero.kerbaltalks.service.UserService;
 import com.chinaero.kerbaltalks.util.HostHolder;
+import com.chinaero.kerbaltalks.util.KerbaltalksConstant;
 import com.chinaero.kerbaltalks.util.KerbaltalksUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * 构造器注入的好处
@@ -22,15 +30,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 
 @Controller
-public class FollowController {
+public class FollowController implements KerbaltalksConstant {
 
     private final FollowService followService;
     private final HostHolder hostHolder;
+    private final UserService userService;
 
 
-    public FollowController(FollowService followService, HostHolder hostHolder) {
+    public FollowController(FollowService followService, HostHolder hostHolder, UserService userService) {
         this.followService = followService;
         this.hostHolder = hostHolder;
+        this.userService = userService;
     }
 
     @RequestMapping(path = "/follow", method = RequestMethod.POST)
@@ -51,6 +61,64 @@ public class FollowController {
         followService.unfollow(user.getId(), entityType, entityId);
 
         return KerbaltalksUtil.getJSONString(1, "已取消关注!");
+    }
+
+    @RequestMapping(path = "/followees/{userId}", method = RequestMethod.GET)
+    public String getFollowees(@PathVariable("userId") int userId, Page page, Model model) {
+        User user = userService.findUserById(userId);
+        if (user == null) {
+            throw new RuntimeException("该用户不存在");
+        }
+        model.addAttribute("user", user);
+
+        System.out.println(page.getClass().getClassLoader());
+
+        page.setLimit(5);
+        page.setPath("/followees/" + userId);
+        page.setRows((int) followService.findFolloweeCount(userId, ENTITY_TYPE_USER));
+
+        List<Map<String, Object>> userList = followService.findFollowees(userId, page.getOffset(), page.getLimit());
+        if (userList != null) {
+            for (Map<String, Object> map : userList) {
+                User u = (User) map.get("user");
+                map.put("hasFollowed", hasFollowed(u.getId()));
+            }
+        }
+        model.addAttribute("users", userList);
+
+        return "/site/followee";
+    }
+
+
+    @RequestMapping(path = "/followers/{userId}", method = RequestMethod.GET)
+    public String getFollowers(@PathVariable("userId") int userId, Page page, Model model) {
+        User user = userService.findUserById(userId);
+        if (user == null) {
+            throw new RuntimeException("该用户不存在!");
+        }
+        model.addAttribute("user", user);
+
+        page.setLimit(5);
+        page.setPath("/followers/" + userId);
+        page.setRows((int) followService.findFollowerCount(ENTITY_TYPE_USER, userId));
+
+        List<Map<String, Object>> userList = followService.findFollowers(userId, page.getOffset(), page.getLimit());
+        if (userList != null) {
+            for (Map<String, Object> map : userList) {
+                User u = (User) map.get("user");
+                map.put("hasFollowed", hasFollowed(u.getId()));
+            }
+        }
+        model.addAttribute("users", userList);
+
+        return "/site/follower";
+    }
+
+    private boolean hasFollowed(int userId) {
+        if (hostHolder.getUser() == null) {
+            return false;
+        }
+        return followService.hasFollowed(hostHolder.getUser().getId(), ENTITY_TYPE_USER, userId);
     }
 
 
--- src/main/java/com/chinaero/kerbaltalks/service/FollowService.java
+++ src/main/java/com/chinaero/kerbaltalks/service/FollowService.java
@@ -3,20 +3,17 @@ package com.chinaero.kerbaltalks.service;
 import com.chinaero.kerbaltalks.entity.User;
 import com.chinaero.kerbaltalks.util.KerbaltalksConstant;
 import com.chinaero.kerbaltalks.util.RedisKeyUtil;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.SessionCallback;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 
 @Service
-public class FollowService {
+public class FollowService implements KerbaltalksConstant {
 
     public final RedisTemplate<String, Object> redisTemplate;
 
@@ -75,17 +72,73 @@ public class FollowService {
         return redisTemplate.opsForZSet().zCard(followeeKey);
     }
 
-    // 查询实体的粉丝的数量
+    /**
+     * 查询实体的粉丝的数量
+     * @param entityType
+     * @param entityId
+     * @return
+     */
     public long findFollowerCount(int entityType, int entityId) {
         String followerKey = RedisKeyUtil.getFollowerKey(entityType, entityId);
         return redisTemplate.opsForZSet().zCard(followerKey);
     }
 
-    // 查询当前用户是否已关注该实体
+    /**
+     * 查询当前用户是否已关注该实体
+     * @param userId
+     * @param entityType
+     * @param entityId
+     * @return
+     */
     public boolean hasFollowed(int userId, int entityType, int entityId) {
         String followeeKey = RedisKeyUtil.getFolloweeKey(userId, entityType);
         return redisTemplate.opsForZSet().score(followeeKey, entityId) != null;
     }
+
+    /**
+     * 查询某用户关注的用户
+     * @param userId 该用户的userId
+     * @param offset 分页Offset
+     * @param limit 分页Limit
+     * @return
+     */
+    public List<Map<String, Object>> findFollowees(int userId, int offset, int limit) {
+        String followeeKey = RedisKeyUtil.getFolloweeKey(userId, ENTITY_TYPE_USER);
+        // 时间倒序
+        return getMaps(offset, limit, followeeKey);
+    }
+
+    /**
+     * 查询某用户的粉丝
+     * @param userId
+     * @param offset
+     * @param limit
+     * @return
+     */
+    public List<Map<String, Object>> findFollowers(int userId, int offset, int limit) {
+        String followerKey = RedisKeyUtil.getFollowerKey(ENTITY_TYPE_USER, userId);
+        return getMaps(offset, limit, followerKey);
+    }
+
+    private List<Map<String, Object>> getMaps(int offset, int limit, String redisKey) {
+        Set<Object> targetIds = redisTemplate.opsForZSet().reverseRange(redisKey, offset, offset + limit - 1);
+        if (targetIds == null) {
+            return null;
+        }
+
+        List<Map<String, Object>> list = new ArrayList<>();
+        for (Object targetId : targetIds) {
+            Map<String, Object> map = new HashMap<>();
+            User user = userService.findUserById((Integer) targetId);
+            map.put("user", user);
+            Double score = redisTemplate.opsForZSet().score(redisKey, targetId);
+            map.put("followTime", new Date(score.longValue()));
+            list.add(map);
+        }
+
+        return list;
+    }
+
 
 
 
